3d3dcbeeadf7713f1670bfe5ee776c7b49198b37
Matthew Sitton <matthewsitton@gmail.com>
2015-10-03 16:32:29 -0500

    Add helper functions for me advanced code generation.

--- opengl/bindutils.py
+++ opengl/bindutils.py
@@ -4,6 +4,49 @@ from ctypes.util import find_library
 import platform
 osName = platform.system()
 
+# list to keep refrences to c strings around
+_stringRef = []
+
+
+def is_sequence(arg):
+    return (not hasattr(arg, "strip") and
+            hasattr(arg, "__getitem__") or
+            hasattr(arg, "__iter__"))
+
+
+def cast_ptr(obj, ptrType):
+    return ct.cast(obj, ct.POINTER(ptrType))
+
+
+def to_c_str(text, extList=False):
+    ''' Convert python strings to null terminated c strings. '''
+    # We only want to keep the cString data around for 1 call
+    if not extList and len(_stringRef) != 0:
+        _stringRef.remove(_stringRef[0])
+    cStr = ct.create_string_buffer(text.encode(encoding='UTF-8'))
+    _stringRef.append(cStr)
+    return ct.cast(ct.pointer(cStr), ct.POINTER(ct.c_char))
+
+
+def conv_list(listIn, cType):
+    ''' Convert a python list into a ctypes array '''
+    return (cType * len(listIn))(*listIn)
+
+
+def conv_list_2d(listIn, cType):
+    ''' Convert a python 2d list into a ctypes 2d array '''
+    xlength = len(listIn)
+    ylength = len(listIn[0])
+
+    arrayOut = (cType * ylength * xlength)()
+
+    for x in range(xlength):
+        for y in range(ylength):
+            arrayOut[x][y] = listIn[x][y]
+
+    return arrayOut
+
+
 def define_function(libName, name, returnType, params):
     '''Helper function to help in binding functions'''
     if osName == "Windows":
@@ -17,6 +60,7 @@ def define_function(libName, name, returnType, params):
     new_func = ct.cast(address, function)
 
     return new_func
+
 
 if osName == "Windows":
     glGetProcAddress = define_function('opengl32', 'wglGetProcAddress',
