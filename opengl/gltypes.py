d8c850491831c15188d1c1e0c9c998743a9cbcb7
Matthew Sitton <matthewsitton@gmail.com>
2015-10-14 16:14:18 -0500

    Add more of the missing opengl types and regen binding.

--- opengl/gltypes.py
+++ opengl/gltypes.py
@@ -1,6 +1,10 @@
 import ctypes as ct
 
+# private non gl types
 _c_ptrdiff_t = ct.c_ssize_t
+
+class _Opaque(ct.Structure):
+    pass
 
 GLenum = ct.c_uint
 GLbitfield = ct.c_uint
@@ -15,17 +19,60 @@ GLushort = ct.c_ushort
 GLulong = ct.c_ulong
 GLfloat = ct.c_float
 GLclampf = ct.c_float
+GLclampx = ct.c_int
 GLdouble = ct.c_double
 GLclampd = ct.c_double
 void = None
 GLvoid = None
 GLchar = ct.c_char
+GLfixed = GLint
 
 GLintptr = _c_ptrdiff_t
+GLintptrARB = _c_ptrdiff_t
+GLsizeiptrARB = _c_ptrdiff_t
 GLsizeiptr = _c_ptrdiff_t
+
+GLvdpauSurfaceNV = GLintptr
+
+GLuint64EXT = ct.c_ulonglong
+GLint64EXT = ct.c_longlong
+
+GLuint64 = ct.c_ulonglong
+GLint64 = ct.c_longlong
+
 
 GLhandleARB = ct.c_uint
 GLcharARB = ct.c_char
 
+GLeglImageOES = ct.c_void_p
+
 GLhalfARB = ct.c_ushort
 GLhalfNV = ct.c_ushort
+GLsync = ct.POINTER(_Opaque)
+
+
+__all__ = ['GLenum',
+        'GLbitfield',
+        'GLuint',
+        'GLint',
+        'GLsizei',
+        'GLboolean',
+        'GLbyte',
+        'GLshort',
+        'GLubyte',
+        'GLushort',
+        'GLulong',
+        'GLfloat',
+        'GLclampf',
+        'GLdouble',
+        'GLclampd',
+        'void',
+        'GLvoid',
+        'GLchar',
+        'GLintptr',
+        'GLsizeiptr',
+        'GLhandleARB',
+        'GLcharARB',
+        'GLhalfARB',
+        'GLhalfNV',
+        'GLsync']
